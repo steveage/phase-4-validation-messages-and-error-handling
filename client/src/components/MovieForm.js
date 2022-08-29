044ccae3191cf52e1c2f0b0b4fb37de047651a16
Mariusz <mariuszbia@yahoo.com>
2022-08-29 11:28:42 -0500

    Completed assignment.

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -23,8 +24,16 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => {
+        if(response.ok) {
+          response.json().then((newMovie) => {
+            console.log(newMovie)
+            setErrors([])
+          });
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      });
   }
 
   function handleChange(e) {
@@ -127,6 +136,15 @@ function MovieForm() {
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
+      {
+        errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )
+      }
     </Wrapper>
   );
 }
